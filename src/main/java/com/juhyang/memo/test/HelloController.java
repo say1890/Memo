2464dda0a0c957880cf17a17e3c6ab1ca7e185db
aa <aa@aa.com>
2022-01-21 21:47:50 +0900

    [최주향] +application.yml, config

--- src/main/java/com/juhyang/memo/test/HelloController.java
+++ src/main/java/com/juhyang/memo/test/HelloController.java
@@ -1,13 +1,12 @@
 package com.juhyang.memo.test;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+
 public class HelloController {
 	@ResponseBody
 	@GetMapping("/hello")
